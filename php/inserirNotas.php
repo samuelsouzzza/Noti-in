59a7e8ff93da252bb55ed246783460a6e8d2aa1c
samuelsouzzza <rssamuel17@gmail.com>
2022-10-11 20:45:09 -0300

    limpando o cod

--- php/inserirNotas.php
+++ php/inserirNotas.php
@@ -5,11 +5,9 @@ $txtTitulo = $_POST['txt-title'];
 $txtDesc = $_POST['txt-descricao'];
 
 $query = "INSERT INTO anotacoes (titulo_notas, descricao_notas) VALUES ('$txtTitulo', '$txtDesc');";
-if(mysqli_query($connect,$query)){
+mysqli_query($connect,$query)){
     echo "gravou";
-}else{
-    echo "n gravou";
-}
+
 
 header('location: ../index.html');
 
