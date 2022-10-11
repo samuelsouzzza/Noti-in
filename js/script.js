86fcafb36fb2404a47831f10adbfeef0dd2be6ef
samuelsouzzza <rssamuel17@gmail.com>
2022-10-11 18:55:16 -0300

    btnFechar pronto js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 const btnCriar = document.querySelector('.add-item');
+const modal = document.querySelector('.modal-wrapper');
+const btnFecharModal = document.querySelector('.btn-fechar-modal');
 
 function criarNota(){
-    const modal = document.querySelector('.modal-wrapper');
+    modal.classList.toggle('ativo');
+}
+function fecharModal(){
     modal.classList.toggle('ativo');
 }
 
 btnCriar.addEventListener('click', criarNota);
+btnFecharModal.addEventListener('click', fecharModal)
