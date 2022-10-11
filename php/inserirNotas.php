946da3646043f25dbfc099425342254cec437beb
samuelsouzzza <rssamuel17@gmail.com>
2022-10-11 20:53:06 -0300

    alteracoes css

--- php/inserirNotas.php
+++ php/inserirNotas.php
@@ -5,7 +5,7 @@ $txtTitulo = $_POST['txt-title'];
 $txtDesc = $_POST['txt-descricao'];
 
 $query = "INSERT INTO anotacoes (titulo_notas, descricao_notas) VALUES ('$txtTitulo', '$txtDesc');";
-mysqli_query($connect,$query)){
+mysqli_query($connect,$query);
     echo "gravou";
 
 
