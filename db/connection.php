6eff2d4421536ce3e88055852eec29ec7dd5e77e
samuelsouzzza <rssamuel17@gmail.com>
2022-10-11 20:15:55 -0300

    conexao php criada e funcionando

--- db/connection.php
+++ db/connection.php
@@ -1,3 +1,16 @@
 <?php
 
+    $hostname = "localhost";
+    $dbname = "minhasnotas";
+    $user = "root";
+    $password= "";
+
+    $pdo = new PDO("mysql:host=$hostname;dbname=$dbname", "$user", "$password"); 
+    
+    
+    // if($pdo){
+    //     echo "Conexão bem-sucedida";
+    // }else{
+    //     echo "Problema na conexão.";
+    // }
 ?>
