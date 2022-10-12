7603d787d387e6b480bceb2d46a7b4c4a26247ee
samuelsouzzza <rssamuel17@gmail.com>
2022-10-11 21:16:21 -0300

    alteracoes na scrollbar do modal

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const btnCriar = document.querySelector('.add-item');
 const modal = document.querySelector('.modal-wrapper');
 const btnFecharModal = document.querySelector('.btn-fechar-modal');
+const txtDesc = document.querySelector('.txt-descricao');
 
 function modalOnOff(){
     modal.classList.toggle('ativo');
+    txtDesc.value = "";
 }
 
 btnCriar.addEventListener('click', modalOnOff);
